03e6357be86dc2c0420c67277a90b819d689ff6f
XifeiNi <cecilia990@outlook.com>
2020-05-01 01:24:02 +1000

    Add frontend build

---
+++ frontend/build/precache-manifest.1bfcd8fb6eb7f62afc5d750a9ea57eeb.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df3cadf080c955f387fadce5a1958023",
+    "url": "/index.html"
+  },
+  {
+    "revision": "811405c4cb1c1a97c542",
+    "url": "/static/css/main.b4d8fc8e.chunk.css"
+  },
+  {
+    "revision": "d7277228e43e765c07ae",
+    "url": "/static/js/2.0104ff1a.chunk.js"
+  },
+  {
+    "revision": "5fb83610db51081ef45599a293cf8ef4",
+    "url": "/static/js/2.0104ff1a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "811405c4cb1c1a97c542",
+    "url": "/static/js/main.9df5db5e.chunk.js"
+  },
+  {
+    "revision": "3f809adccd7b5eb81ce7",
+    "url": "/static/js/runtime-main.6d8ceafa.js"
+  },
+  {
+    "revision": "d3085f686df272f9e1a267cc69b2d24f",
+    "url": "/static/media/Montserrat-Bold.d3085f68.ttf"
+  },
+  {
+    "revision": "07689d4eaaa3d530d58826b5d7f84735",
+    "url": "/static/media/Montserrat-Regular.07689d4e.ttf"
+  },
+  {
+    "revision": "39e651300c0dc627e5d9309dd99f5cf9",
+    "url": "/static/media/arrow_right_black.39e65130.png"
+  },
+  {
+    "revision": "c494cbb33b417a016662d2a2573578ea",
+    "url": "/static/media/arrow_right_white.c494cbb3.png"
+  },
+  {
+    "revision": "94078efbb3763ddea89169c143ab23c6",
+    "url": "/static/media/background_day.94078efb.png"
+  },
+  {
+    "revision": "6cd10209342be1467783613d442c09ab",
+    "url": "/static/media/background_evening.6cd10209.png"
+  },
+  {
+    "revision": "17de83ed987e5c3dd593161c64316993",
+    "url": "/static/media/background_rainy.17de83ed.png"
+  },
+  {
+    "revision": "0f0409126755d1e80e89ac3c9f6c9ae3",
+    "url": "/static/media/background_sunrise.0f040912.png"
+  },
+  {
+    "revision": "5bb43f5317d02ad72a1f1f4d7bbff1ba",
+    "url": "/static/media/background_sunset.5bb43f53.png"
+  },
+  {
+    "revision": "88c9750bdbabc6e4e35e9a24bab2033e",
+    "url": "/static/media/closing_icon_black.88c9750b.png"
+  }
+]);
